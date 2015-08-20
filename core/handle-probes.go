4c2598a76ae2e6e6f32d9b25c471ff61c1ad3e86
Nurza <gabriel.mgt0+git@gmail.com>
2015-08-20 13:48:24 +0200

    Improve HTTPHandlerProbes

--- core/handle-probes.go
+++ core/handle-probes.go
@@ -7,16 +7,35 @@ import (
 )
 
 /*
-	Return the probes map
+	Simplified Probe struct
+*/
+type SProbe struct {
+	ID   int    `yaml:"id"`
+	Name string `yaml:"name"`
+}
+
+/*
+	Return simplified probes array
 */
 func HTTPHandlerProbes(w http.ResponseWriter, r *http.Request) {
-	var returnStr string // HTTP Response body
+	var returnStr string      // HTTP Response body
+	var returnProbes []SProbe // Returned simplified probes
+	var err error             // Error handling
+
+	// Get simplified probes
+	for key, probeID := range ProbesID {
+		returnProbes = append(returnProbes, SProbe{probeID, key})
+	}
 
 	// probes => json
-	tmpJson, _ := json.Marshal(ProbesID)
+	tmpJson, err := json.Marshal(returnProbes)
+	if err != nil {
+		l.Error("HTTPHandlerProbes: Failed to marshal struct")
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
 
 	// Add json to the returned string
 	returnStr = string(tmpJson)
-
 	fmt.Fprint(w, returnStr)
 }
