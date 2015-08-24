c98309e86a50a47185adafdd77f68f260a355fc5
Nurza <gabriel.mgt0+git@gmail.com>
2015-08-24 10:54:21 +0200

    Add easy to make transport

--- core/event.go
+++ core/event.go
@@ -1,24 +1,39 @@
 package core
 
 import (
+	"os/exec"
+
+	"../utils"
+
 	dguard "github.com/90TechSAS/libgo-docker-guard"
 )
 
 var ()
 
-type Event dguard.Event
-
 type Transport struct {
 	Name string `yaml:"name"`
 	Path string `yaml:"path"`
-	Ip   string `yaml:"ip"`
-	Port string `yaml:"port"`
 }
 
 func EventController() {
 	// TODO
 }
 
-func (e *Event) Alert() {
-	// TODO
+func Alert(event dguard.Event) {
+	var err error  // Error handling
+	var out []byte // Command output
+
+	// Exec transport
+	out, err = exec.Command("./transports/TRANSPORT.sh",
+		utils.I2S(event.Severity),
+		event.TypeToString(),
+		event.Target,
+		event.Probe,
+		event.Data).Output()
+	if err != nil {
+		l.Error("Out:", string(out))
+		return
+	}
+
+	l.Info("Out:", string(out))
 }
---
+++ transports/slack.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Configuration
+HOOK_URL="https://hooks.slack.com/services/YOURKEY"
+CHANNEL="#mychannel"
+USERNAME="DGS"
+ICON=":squirrel:"
+
+# Check arguments
+if [ $# -lt 5 ] || [ $# -gt 5 ] ; then
+        echo "Error, usage: slack.sh <severity> <type> <target> <target_probe> <data>"
+        echo "severity	Severity levels:"
+        echo "				0: Notice"
+        echo "				1: Warning"
+        echo "				2: Critical"
+        echo 
+        echo "Type 		Alert types:"
+        echo "				DiskSpaceLimitReached"
+        echo "				MemorySpaceLimitReached"
+        echo "				ContainerStarted"
+        echo "				ContainerStopped"
+        echo "				ContainerRemoved"
+        echo "				DiskIOOverload"
+        echo "				NetBandwithOverload"
+        echo "				CPUUsageOverload"
+        echo 
+        echo "target		Targeted system(s)"
+        echo 
+        echo "target_probe	Name of the target's probe"
+        echo
+        echo "data		Additional data"
+        echo
+        echo "Example: slack.sh 3 DiskSpaceLimitReached db-1 production '20GB/20GB'"
+        echo
+        echo "Used: $0 $@"
+        exit 1
+fi
+
+# Set variables
+COLOR=""
+SEVERITY=""
+COLOR_NOTICE="#05c1ff"
+COLOR_WARNING="#ffff00"
+COLOR_CRITICAL="#ff0000"
+
+case "$1" in
+	0)
+		SEVERITY="NOTICE"
+		COLOR=$COLOR_NOTICE		
+		;;
+	1)
+		SEVERITY="WARNING"
+		COLOR=$COLOR_WARNING
+		;;
+	2)
+		SEVERITY="CRITICAL"
+		COLOR=$COLOR_CRITICAL
+		;;
+	*)
+		echo "Error: Severity unknow"
+		exit 1
+		;;
+esac
+
+TEXT="\"attachments\": [{
+            \"pretext\": \"New $SEVERITY $2 alert:\",
+            \"text\": \"\",
+            \"fields\": [
+                {
+                    \"title\": \"Target(s)\",
+                    \"value\": \"$3\",
+                    \"short\": true
+                },
+                {
+                    \"title\": \"Probe\",
+                    \"value\": \"$4\",
+                    \"short\": true
+                },
+                {
+                    \"title\": \"Additional data\",
+                    \"value\": \"$5\"
+                }
+            ],
+            \"color\": \"$COLOR\"
+        }
+    ]"
+
+PAYLOAD="{\"channel\":\"$CHANNEL\",\"username\":\"$USERNAME\",$TEXT,\"icon_emoji\":\"$ICON\"}"
+
+# Do the HTTP POST request to slack
+curl -X POST \
+	--data-urlencode "payload=$PAYLOAD" \
+	$HOOK_URL
